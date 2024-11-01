c9f1ff8a8e9897ef49fc26c3d783fbd5c364da56
Mason Pham <59492883+masonpham16@users.noreply.github.com>
2024-11-01 17:47:07 -0400

    Create romanToInt.java

---
+++ romanToInt.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i-1))) {
+                total += map.get(s.charAt(i)) - 2*map.get(s.charAt(i-1));
+            } else {
+                total += map.get(s.charAt(i));
+            }
+        }
+        return total;
+    }
+}
