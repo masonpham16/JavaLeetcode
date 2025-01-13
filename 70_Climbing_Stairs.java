a5a77710ebaa1f703325cb51ee2e79b863b80d62
Mason Pham <59492883+masonpham16@users.noreply.github.com>
2025-01-13 14:49:48 -0500

    Update 70_Climbing_Stairs.java

--- 70_Climbing_Stairs.java
+++ 70_Climbing_Stairs.java
@@ -13,3 +13,25 @@ class Solution {
         return dp[n];
         }
 }
+
+
+
+/*
+Example Walkthrough for n=5
+    Initialize dp[]
+            dp=[0,1,2,0,0,0]
+            
+    Calculate dp[3]
+        dp[3]=dp[2]+dp[1]=2+1=3
+            dp=[0,1,2,3,0,0]
+            
+    Calculate dp[4]
+        dp[4]=dp[3]+dp[2]=3+2=5
+            dp=[0,1,2,3,5,0]
+            
+    Calculate dp[5]
+        dp[5]=dp[4]+dp[3]=5+3=8
+            dp=[0,1,2,3,5,8]
+            
+Finally, return dp[5]=8
+*/
