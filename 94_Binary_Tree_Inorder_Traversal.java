1819ef6a0238d7afe81dea19af17c6d9af0282e6
Mason Pham <59492883+masonpham16@users.noreply.github.com>
2025-01-31 00:02:01 -0500

    Create 94_Binary_Tree_Inorder_Traversal.java

---
+++ 94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inOrder(root, result);
+        return result;
+    }
+
+    private void inOrder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, result);   // Visit left subtree
+        result.add(root.val);         // Visit root node
+        inOrder(root.right, result);  // Visit right subtree
+    } 
+}
